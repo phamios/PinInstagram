ee9d6f65a65f0dfb275848ffaf7ccf8058561f5e
phamios <sonpxvn@gmail.com>
2018-02-08 17:29:33 +0600

    update

--- InstagramTool/Form1.Designer.cs
+++ InstagramTool/Form1.Designer.cs
@@ -33,19 +33,25 @@
             this.button2 = new System.Windows.Forms.Button();
             this.listView2 = new System.Windows.Forms.ListView();
             this.button3 = new System.Windows.Forms.Button();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 44);
+            this.listView1.Location = new System.Drawing.Point(12, 55);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(183, 240);
+            this.listView1.Size = new System.Drawing.Size(183, 211);
             this.listView1.TabIndex = 5;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 292);
+            this.button1.Location = new System.Drawing.Point(12, 26);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(115, 23);
             this.button1.TabIndex = 6;
@@ -55,35 +61,96 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(201, 290);
+            this.button2.Location = new System.Drawing.Point(201, 272);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(123, 23);
             this.button2.TabIndex = 7;
             this.button2.Text = "Import List File";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listView2
             // 
-            this.listView2.Location = new System.Drawing.Point(201, 44);
+            this.listView2.Location = new System.Drawing.Point(201, 100);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(418, 240);
+            this.listView2.Size = new System.Drawing.Size(418, 166);
             this.listView2.TabIndex = 8;
             this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(13, 12);
+            this.button3.Location = new System.Drawing.Point(500, 7);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(606, 24);
+            this.button3.Size = new System.Drawing.Size(119, 42);
             this.button3.TabIndex = 9;
             this.button3.Text = "UPLOAD ALL";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(12, 308);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(607, 110);
+            this.listView3.TabIndex = 10;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.List;
+            this.listView3.SelectedIndexChanged += new System.EventHandler(this.listView3_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 39);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "HashTag";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Current Log";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 451);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Devices:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 441);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(236, 23);
+            this.progressBar1.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 327);
+            this.ClientSize = new System.Drawing.Size(631, 476);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.button2);
@@ -93,6 +160,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,6 +171,12 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- InstagramTool/Form1.cs
+++ InstagramTool/Form1.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using InstaSharp;
+using InstaSharp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,35 +25,11 @@ namespace InstagramTool
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            
             openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            if (File.Exists(openFileDialog.FileName))
-            {
-                
-                    var lines = File.ReadAllLines(openFileDialog.FileName);
-                    if (lines.Length < 3)
-                        MessageBox.Show("Error: not enough lines in file: " + openFileDialog.FileName);
-                    else
-                    {
-                        InvoiceNumbertxt.Text = lines[0];
-                        InvoiceDatetxt.Text = lines[1];
-                        DueDatetxt.Text = lines[2];
-                        foreach (var l in lines.Skip(3).Take(6))
-                            PersonInfolst.Items.Add(l);
-
-                        // I am assuming that the `ItemProperties` follow, as groups of 3 lines,
-                        // consisting of `Item`, `Description`, `Publisher`
-                        for (var i = 9; i + 2 < lines.Length; i += 3)
-                        {
-                            Items.Add(new ItemProperties
-                            {
-                                Item = lines[i],
-                                Description = lines[i + 1],
-                                Publisher = lines[i + 2]
-                            });
-                        }
-                        itemlst.ItemsSource = Items;
-                    } 
-            }
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.ShowDialog();
+            ReadInTimeSheet(openFileDialog.FileName);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -57,7 +37,6 @@ namespace InstagramTool
             listView1.View = View.Details;
             listView1.Columns.Add("Username");
             listView1.Columns.Add("Password"); 
-
             // Auto-size the columns
             for (int i = 0; i < listView1.Columns.Count; i++)
             {
@@ -66,28 +45,195 @@ namespace InstagramTool
         }
 
 
-        private void ReadInTimeSheet()
+        private void ReadInTimeSheet(String filename)
         {
 
-            var fileLines = File.ReadAllLines(@"C:\filepath\MyTimeSheet.txt");
+            var fileLines = File.ReadAllLines( filename);
 
-                for (int i = 0; i + 4 < fileLines.Length; i += 5)
+                for (int i = 0; i  < fileLines.Length; i++)
                     {
+                        String[] acc = fileLines[i].Split('|');
+                        
                         listView1.Items.Add(
                             new ListViewItem(new[]
                             {
-                                fileLines[i],
-                                fileLines[i + 1], 
+                                acc[0],
+                                acc[1], 
                             }));
                     }
 
-                    // Resize the columns
-                    for (int i = 0; i < listView1.Columns.Count; i++)
+            // Resize the columns
+            for (int i = 0; i < listView1.Columns.Count; i++)
+            {
+                listView1.Columns[i].Width = -2;
+            }
+        }
+
+
+
+
+        public static SecureString ConvertToSecureString(string strPassword)
+        {
+            var secureStr = new SecureString();
+            if (strPassword.Length > 0)
+            {
+                foreach (var c in strPassword.ToCharArray()) secureStr.AppendChar(c);
+            }
+            return secureStr;
+        }
+
+
+
+
+        private  void OnMediaUploadStartedEvent(object sender, EventArgs e)
+        {
+            listView3.Items.Add("Attempting to upload image");
+            
+            
+        }
+
+        private  void OnmediaUploadCompleteEvent(object sender, EventArgs e)
+        {
+            listView3.Items.Add("The image was uploaded, but has not been configured yet.");
+        }
+
+
+        private  void OnMediaConfigureStarted(object sender, EventArgs e)
+        {
+            listView3.Items.Add("The image has started to be configured");
+        }
+
+        private  void SuccessfulLoginEvent(object sender, EventArgs e)
+        {
+            listView3.Items.Add("Logged in! " + ((LoggedInUserResponse)e).FullName);
+        }
+
+        private  void OnLoginEvent(object sender, EventArgs e)
+        {
+            listView3.Items.Add("Event fired for login: " + ((NormalResponse)e).Message);
+        }
+
+        private  void OnCompleteEvent(object sender, EventArgs e)
+        {
+
+            listView3.Items.Add("Image posted to Instagram, here are all the urls");
+            foreach (var image in ((UploadResponse)e).Images)
+            {
+                listView3.Items.Add("Uploaded: " + image.Url + "  (Size: " + image.Width + "|" + image.Height + ")");
+            }
+        }
+
+        private  void ErrorEvent(object sender, EventArgs e)
+        {
+            listView3.Items.Add("Error  " + ((ErrorResponse)e).Message);
+        }
+
+        private  void InvalidLoginEvent(object sender, EventArgs e)
+        {
+            listView3.Items.Add("Error while logging  " + ((ErrorResponse)e).Message);
+        }
+
+        private void listView3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(runPush);          // Kick off a new thread
+            t.Start();
+        }
+
+        void runPush()
+        {
+            if (listView2.InvokeRequired)
+            {
+                listView2.Invoke((MethodInvoker)delegate ()
+                {
+                    progressBar1.Maximum = listView2.Items.Count;
+                });
+            }
+
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+
+                if (listView1.InvokeRequired)
+                {
+                    listView1.Invoke((MethodInvoker)delegate ()
                     {
-                        listView1.Columns[i].Width = -2;
+                        
+                         //listView1.Items[i].SubItems[0].Text
+                         var uploader = new InstagramUploader(listView1.Items[i].SubItems[0].Text, ConvertToSecureString(listView1.Items[i].SubItems[1].Text));
+                        uploader.InvalidLoginEvent += InvalidLoginEvent;
+                        uploader.ErrorEvent += ErrorEvent;
+                        uploader.OnCompleteEvent += OnCompleteEvent;
+                        uploader.OnLoginEvent += OnLoginEvent;
+                        uploader.SuccessfulLoginEvent += SuccessfulLoginEvent;
+                        uploader.OnMediaConfigureStarted += OnMediaConfigureStarted;
+                        uploader.OnMediaUploadStartedEvent += OnMediaUploadStartedEvent;
+                        uploader.OnMediaUploadeComplete += OnmediaUploadCompleteEvent;
+
+                        if (listView2.InvokeRequired)
+                        {
+                            listView2.Invoke((MethodInvoker)delegate ()
+                            {
+                                for (int image = 0; image < listView2.Items.Count; image++)
+                                {
+                                    progressBar1.Value = image +1 ;
+                                    uploader.UploadImage(listView2.Items[image].Text, textBox1.Text);
+                                }
+                            });
+                        }
+                        
+                        
+                    });
+                   
+                }
+
+                if (label3.InvokeRequired)
+                {
+                    label3.Invoke((MethodInvoker)delegate ()
+                    {
+                        label3.Text = "Your DeviceID is: " + InstaSharp.Properties.Settings.Default.deviceId;
+                    });
+                }
+
+
+                
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream; 
+            OpenFileDialog thisDialog = new OpenFileDialog();
+            thisDialog.Filter = "Image File (*.jpg)|*.jpg|All files (*.*)|*.*";
+            thisDialog.RestoreDirectory = true;
+            thisDialog.Multiselect = true;
+
+            if (thisDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in thisDialog.FileNames)
+                {
+                    try
+                    {
+                        if ((myStream = thisDialog.OpenFile()) != null)
+                        {
+                            using (myStream)
+                            {
+                                listView2.Items.Add(file);
+                            }
+                        }
                     }
-       }
 
-
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    }
+                }
+            }
+        }
     }
 }
