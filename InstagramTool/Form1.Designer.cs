979f9db1e2821206cbc8e5c3106410dfb75df756
phamios <sonpxvn@gmail.com>
2018-02-08 16:07:27 +0600

    first commit

---
+++ InstagramTool/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace InstagramTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 44);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(183, 240);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Import Account";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Import List File";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(201, 44);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(418, 240);
+            this.listView2.TabIndex = 8;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(606, 24);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "UPLOAD ALL";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 327);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ InstagramTool/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstagramTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (File.Exists(openFileDialog.FileName))
+            {
+                
+                    var lines = File.ReadAllLines(openFileDialog.FileName);
+                    if (lines.Length < 3)
+                        MessageBox.Show("Error: not enough lines in file: " + openFileDialog.FileName);
+                    else
+                    {
+                        InvoiceNumbertxt.Text = lines[0];
+                        InvoiceDatetxt.Text = lines[1];
+                        DueDatetxt.Text = lines[2];
+                        foreach (var l in lines.Skip(3).Take(6))
+                            PersonInfolst.Items.Add(l);
+
+                        // I am assuming that the `ItemProperties` follow, as groups of 3 lines,
+                        // consisting of `Item`, `Description`, `Publisher`
+                        for (var i = 9; i + 2 < lines.Length; i += 3)
+                        {
+                            Items.Add(new ItemProperties
+                            {
+                                Item = lines[i],
+                                Description = lines[i + 1],
+                                Publisher = lines[i + 2]
+                            });
+                        }
+                        itemlst.ItemsSource = Items;
+                    } 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("Username");
+            listView1.Columns.Add("Password"); 
+
+            // Auto-size the columns
+            for (int i = 0; i < listView1.Columns.Count; i++)
+            {
+                listView1.Columns[i].Width = -2;
+            }
+        }
+
+
+        private void ReadInTimeSheet()
+        {
+
+            var fileLines = File.ReadAllLines(@"C:\filepath\MyTimeSheet.txt");
+
+                for (int i = 0; i + 4 < fileLines.Length; i += 5)
+                    {
+                        listView1.Items.Add(
+                            new ListViewItem(new[]
+                            {
+                                fileLines[i],
+                                fileLines[i + 1], 
+                            }));
+                    }
+
+                    // Resize the columns
+                    for (int i = 0; i < listView1.Columns.Count; i++)
+                    {
+                        listView1.Columns[i].Width = -2;
+                    }
+       }
+
+
+    }
+}
